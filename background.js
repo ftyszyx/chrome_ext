3a282c3f05f2018233bb865be1de2bf15f7165e1
ftyszyx <whyzi@qq.com>
2015-08-01 16:24:44 +0800

    不打开已经访问的页面

--- background.js
+++ background.js
@@ -3,7 +3,18 @@ chrome.extension.onRequest.addListener(
     function(request,sender,sendResponse){
         console.log("get a request");
         for(var i=0;i<request.length;i++){ 
-            chrome.tabs.create(request[i]);      
-        }
-        
+            //只打开没访问过的
+            var detail={};
+            detail.url=request[i].url;
+
+            var processVisitUrl=function(request_info){
+                return function(visitItems){
+                    if(visitItems.length==0){
+                        chrome.tabs.create(request_info); 
+                        console.log("visit:"+request_info.url);                      
+                    }
+                }
+            }
+            chrome.history.getVisits(detail,processVisitUrl(request[i]));
+        }        
 });
